d1658fc579ceda3b93d0c0a507c6fa7896efe60d
yadav-sanjana <sanjanay751@gmail.com>
2023-12-05 15:38:17 +0530

    landing page

--- utils/Card.tsx
+++ utils/Card.tsx
@@ -11,15 +11,15 @@ const HelpCard: React.FC<HelpCardProps> = ({ title, content, icon: Icon }) => {
     return (
         <>
 
-            <div className="max-w-sm hover:bg-cyan-100 p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <div className="max-w-sm w-full h-full hover:bg-cyan-100 p-6 bg-white border border-gray-200 rounded-lg shadow">
 
                 <div className="items-center">
                     <Icon className="h-8 w-8 text-cyan-500" />
                     <a href="#">
-                        <p className="my-2  text-2xl font-semibold tracking-tight text-gray-900 dark:text-gray-600">{title}</p>
+                        <p className="my-2  text-2xl font-semibold tracking-tight text-gray-900">{title}</p>
                     </a>
                 </div>
-                <p className="mb-2 font-normal text-gray-500">{content}</p>
+                <p className="mb-2 min-h-10 font-normal text-gray-500">{content}</p>
             </div >
             
 
